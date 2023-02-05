7b21d301060f4d2629a764dc7af296464cbb5f9d
Shane Smith <shane.smith@skyteam.com>
2023-02-05 21:10:46 +0100

    add basic nft view setup

---
+++ src/views/nfts/index.tsx
@@ -0,0 +1,19 @@
+
+import { FC } from "react";
+
+export const NFTView: FC = ({ }) => {
+
+  return (
+<div className="md:hero mx-auto p-4">
+      <div className="md:hero-content flex flex-col">
+        <h1 className="text-center text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-tr from-[#9945FF] to-[#14F195]">
+          NFT view
+        </h1>
+        {/* CONTENT GOES HERE */}
+        <div className="text-center">
+
+        </div>
+      </div>
+    </div>
+  );
+};
